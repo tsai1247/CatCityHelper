a6a8f85a6699900bcd5fb99b2897806f3951cc82
tsai1247 <jeff29cc91079@gmail.com>
2023-12-21 00:30:27 +0800

    feat: set root route path

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/CatCity',
+    path: '/CatCityHelper',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
--- vite.config.js
+++ vite.config.js
@@ -47,5 +47,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: "/CatCity"
+  base: "/CatCityHelper"
 })
