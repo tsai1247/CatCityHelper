549524dbb29961e0727a60f07c540e8f20572a43
tsai1247 <jeff29cc91079@gmail.com>
2024-01-15 17:10:49 +0800

    feat: all character images and import js file

---
+++ src/common/images.js
@@ -0,0 +1,149 @@
+import AI助理 from "../assets/AI助理.jpg"
+import 不思議鴨鴨 from "../assets/不思議鴨鴨.jpg"
+import 修習者 from "../assets/修習者.jpg"
+import 冬季奇遇 from "../assets/冬季奇遇.jpg"
+import 命運審判 from "../assets/命運審判.jpg"
+import 噬血魔女 from "../assets/噬血魔女.jpg"
+import 囚鳥綺願 from "../assets/囚鳥綺願.jpg"
+import 墨蘊尋真 from "../assets/墨蘊尋真.jpg"
+import 夕雪晚照 from "../assets/夕雪晚照.jpg"
+import 夜間執勤 from "../assets/夜間執勤.jpg"
+import 奪魂之鐮 from "../assets/奪魂之鐮.jpg"
+import 孤獨紳士 from "../assets/孤獨紳士.jpg"
+import 守夜人 from "../assets/守夜人.jpg"
+import 完美效勞 from "../assets/完美效勞.jpg"
+import 封面女郎 from "../assets/封面女郎.jpg"
+import 幽靈社團 from "../assets/幽靈社團.jpg"
+import 心動療癒 from "../assets/心動療癒.jpg"
+import 怪力巨兔 from "../assets/怪力巨兔.jpg"
+import 恬靜時刻 from "../assets/恬靜時刻.jpg"
+import 惡念殘響 from "../assets/惡念殘響.jpg"
+import 愛心妝點 from "../assets/愛心妝點.jpg"
+import 憎惡鬥者 from "../assets/憎惡鬥者.jpg"
+import 塗鴉戰爭 from "../assets/塗鴉戰爭.jpg"
+import 戰略潛艇 from "../assets/戰略潛艇.jpg"
+import 搬運工 from "../assets/搬運工.jpg"
+import 星之願 from "../assets/星之願.jpg"
+import 時運失衡 from "../assets/時運失衡.jpg"
+import 暗巷星火 from "../assets/暗巷星火.jpg"
+import 枕邊童話 from "../assets/枕邊童話.jpg"
+import 棋盤遊戲 from "../assets/棋盤遊戲.jpg"
+import 森林守護者 from "../assets/森林守護者.jpg"
+import 極寒朔風 from "../assets/極寒朔風.jpg"
+import 橘色純真 from "../assets/橘色純真.jpg"
+import 機械助理 from "../assets/機械助理.jpg"
+import 海灘冒險 from "../assets/海灘冒險.jpg"
+import 清道夫之夜 from "../assets/清道夫之夜.jpg"
+import 滌蕩者 from "../assets/滌蕩者.jpg"
+import 瀕死血液 from "../assets/瀕死血液.jpg"
+import 火樹流光 from "../assets/火樹流光.jpg"
+import 烈火戰線 from "../assets/烈火戰線.jpg"
+import 爆裂火山 from "../assets/爆裂火山.jpg"
+import 狂想花骸 from "../assets/狂想花骸.jpg"
+import 瘋人院 from "../assets/瘋人院.jpg"
+import 癔症粉絲 from "../assets/癔症粉絲.jpg"
+import 白瑰綺晶 from "../assets/白瑰綺晶.jpg"
+import 碧魂燈 from "../assets/碧魂燈.jpg"
+import 秘域訪客 from "../assets/秘域訪客.jpg"
+import 秘密調查 from "../assets/秘密調查.jpg"
+import 粉紅風暴 from "../assets/粉紅風暴.jpg"
+import 紀律監督 from "../assets/紀律監督.jpg"
+import 純真拜訪者 from "../assets/純真拜訪者.jpg"
+import 緋紅武士 from "../assets/緋紅武士.jpg"
+import 繼承者 from "../assets/繼承者.jpg"
+import 自由輪滑 from "../assets/自由輪滑.jpg"
+import 致命音符 from "../assets/致命音符.jpg"
+import 致聖雪夜 from "../assets/致聖雪夜.jpg"
+import 英雄憧憬 from "../assets/英雄憧憬.jpg"
+import 落寞淑女 from "../assets/落寞淑女.jpg"
+import 蒼白舞者 from "../assets/蒼白舞者.jpg"
+import 詐欺小丑 from "../assets/詐欺小丑.jpg"
+import 銀鉤墨舞 from "../assets/銀鉤墨舞.jpg"
+import 鋼鐵傀儡 from "../assets/鋼鐵傀儡.jpg"
+import 鏡光旖夢 from "../assets/鏡光旖夢.jpg"
+import 長夜冰冕 from "../assets/長夜冰冕.jpg"
+import 除障者 from "../assets/除障者.jpg"
+import 障礙破壞 from "../assets/障礙破壞.jpg"
+import 雪地奔馳 from "../assets/雪地奔馳.jpg"
+import 電玩怪客 from "../assets/電玩怪客.jpg"
+import 電音騎士 from "../assets/電音騎士.jpg"
+import 青之迴旋 from "../assets/青之迴旋.jpg"
+import 高級特工 from "../assets/高級特工.jpg"
+
+const characterImages = {
+  AI助理,
+  不思議鴨鴨,
+  修習者,
+  冬季奇遇,
+  命運審判,
+  噬血魔女,
+  囚鳥綺願,
+  墨蘊尋真,
+  夕雪晚照,
+  夜間執勤,
+  奪魂之鐮,
+  孤獨紳士,
+  守夜人,
+  完美效勞,
+  封面女郎,
+  幽靈社團,
+  心動療癒,
+  怪力巨兔,
+  恬靜時刻,
+  惡念殘響,
+  愛心妝點,
+  憎惡鬥者,
+  塗鴉戰爭,
+  戰略潛艇,
+  搬運工,
+  星之願,
+  時運失衡,
+  暗巷星火,
+  枕邊童話,
+  棋盤遊戲,
+  森林守護者,
+  極寒朔風,
+  橘色純真,
+  機械助理,
+  海灘冒險,
+  清道夫之夜,
+  滌蕩者,
+  瀕死血液,
+  火樹流光,
+  烈火戰線,
+  爆裂火山,
+  狂想花骸,
+  瘋人院,
+  癔症粉絲,
+  白瑰綺晶,
+  碧魂燈,
+  秘域訪客,
+  秘密調查,
+  粉紅風暴,
+  紀律監督,
+  純真拜訪者,
+  緋紅武士,
+  繼承者,
+  自由輪滑,
+  致命音符,
+  致聖雪夜,
+  英雄憧憬,
+  落寞淑女,
+  蒼白舞者,
+  詐欺小丑,
+  銀鉤墨舞,
+  鋼鐵傀儡,
+  鏡光旖夢,
+  長夜冰冕,
+  除障者,
+  障礙破壞,
+  雪地奔馳,
+  電玩怪客,
+  電音騎士,
+  青之迴旋,
+  高級特工,
+}
+
+export default {
+  characterImages
+}
