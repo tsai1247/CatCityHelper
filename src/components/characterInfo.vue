3a90f0a953eab7edb5c10c3a0ce3eb98825a7585
tsai1247 <jeff29cc91079@gmail.com>
2024-01-15 18:50:57 +0800

    fix: character info extends out of the screen

--- src/components/characterInfo.vue
+++ src/components/characterInfo.vue
@@ -1,21 +1,25 @@
 <template>
-  <v-card v-if="character"
-    class="ma-2"
-    width="100%"
-    height="100%">
+  <v-row>
+    <v-col>
+      <v-card v-if="character"
+        class="ma-2 fixed-element"
+        width="100%"
+        height="100%">
 
-      <template v-slot:title>
-        <span>{{character.name}}</span>
-        <v-icon class="float-right"
-          @click="closeDialog">
-          mdi-close-circle
-        </v-icon>
-      </template>
+          <template v-slot:title>
+            <span>{{character.name}}</span>
+            <v-icon class="float-right"
+              @click="closeDialog">
+              mdi-close-circle
+            </v-icon>
+          </template>
 
-      <v-card-text>
-        <span>{{character.name}} 的資訊</span>
-      </v-card-text>
-  </v-card>
+          <v-card-text>
+            <span>{{character.name}} 的資訊</span>
+          </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup>
