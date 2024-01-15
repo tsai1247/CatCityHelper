fca7e5fce89fde5c689b8c97d24c2417a10294b4
tsai1247 <jeff29cc91079@gmail.com>
2024-01-16 01:02:33 +0800

    feat: add particle search filter

--- src/components/characterView.vue
+++ src/components/characterView.vue
@@ -5,7 +5,7 @@
       <v-row>
         <v-col cols="9">
           <v-row>
-            <v-col cols="12" md="4">
+            <v-col cols="12">
               <v-text-field
                 v-model="keyword"
                 clearable
@@ -13,7 +13,7 @@
 
               </v-text-field>
             </v-col>
-            <v-col cols="12" sm="7" md="4">
+            <v-col cols="12" md="7" lg="5">
               <v-btn-toggle
                 v-model="attributeFilter"
                 dark
@@ -27,7 +27,21 @@
                 </v-btn>
               </v-btn-toggle>
             </v-col>
-            <v-col cols="12" sm="5" md="4">
+            <v-col cols="12" md="5" lg="3">
+              <v-btn-toggle
+                v-model="particleFilter"
+                dark
+                multiple>
+                <v-btn v-for="(item) in particleEnum"
+                  @click="filter"
+                  :key="item.id"
+                  density="compact"
+                >
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-btn>
+              </v-btn-toggle>
+            </v-col>
+            <v-col cols="12" md="12" lg="3">
               <v-btn-toggle
                 v-model="levelFilter"
                 dark
@@ -66,19 +80,28 @@
 
   const levelEnum = commonEnum.level;
   const attributeEnum = commonEnum.attribute;
+  const particleEnum = commonEnum.particle;
   const getObjKeys = enumRelated.getObjKeys;
 
   const characterList = ref(character);
   const levelFilter = ref([0, 1, 2]);
   const attributeFilter = ref([0, 1, 2, 3, 4]);
+  const particleFilter = ref([0, 1, 2, 3]);
   const keyword = ref("");
 
   function filter() {
     characterList.value = character.filter(
       (item) => {
-        const levelResult = levelFilter.value.find(level => item.level === level);
-        const attributeResult = attributeFilter.value.find((attribute) => item.attribute?.id === attribute);
-        return levelResult !== undefined && attributeResult !== undefined || item.attribute === undefined;
+        const results = [
+          item.level === null || levelFilter.value.find(level => item.level === level) !== undefined,
+          item.attribute === null || attributeFilter.value.find((attribute) => item.attribute?.id === attribute)  !== undefined,
+          item.particle === null || particleFilter.value.find((particle) => item.particle?.id === particle)  !== undefined
+        ];
+
+
+        return results.reduce(
+          (result, cur) => result && cur, true
+        );
       }
     );
 
