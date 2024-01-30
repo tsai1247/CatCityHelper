69b70c247058b5223f8051d6d7247d5fc4f57ddf
tsai1247 <jeff29cc91079@gmail.com>
2024-01-30 22:10:23 +0800

    feat: foldable search field

--- src/components/searchField.vue
+++ src/components/searchField.vue
@@ -1,48 +1,71 @@
 <template>
+  <v-row>
+    <!-- search text field -->
+    <v-col cols="10" md="11" >
       <v-row>
-        <!-- search text field -->
-        <v-col cols="12">
-          <v-row>
-            <!-- character name -->
-            <v-col cols="12" md="5">
-              <v-text-field
-                label="角色名稱"
-                v-model="nameKeyword"
-                clearable
-                prepend-inner-icon="mdi-magnify">
-              </v-text-field>
-            </v-col>
-
-            <!-- character skill -->
-            <v-col cols="12" md="5">
-              <v-text-field
-                v-if="isSelfDefinedSkill"
-                label="技能敘述-其他"
-                v-model="skillKeyword"
-                clearable
-                prepend-inner-icon="mdi-magnify"
-                append-inner-icon="mdi-dots-horizontal"
-                @click:append-inner="changeSkillKeywordType">
-              </v-text-field>
-              <v-autocomplete
-                v-else
-                label="技能敘述"
-                v-model="skillKeyword"
-                prepend-inner-icon="mdi-magnify"
-                append-inner-icon="mdi-dots-horizontal"
-                :items="skillDescription"
-                item-value="name"
-                clearable
-                :item-props="(item) => {return {'title': item.name, 'subtitle': item.description}}"
-
-                @click:append-inner="changeSkillKeywordType"
-              >
-
-              </v-autocomplete>
-            </v-col>
-          </v-row>
+        <!-- character name -->
+        <v-col cols="12" :md="isExpanded ? 6 : 12">
+          <v-text-field
+            label="角色名稱"
+            v-model="nameKeyword"
+            clearable
+            prepend-inner-icon="mdi-magnify">
+          </v-text-field>
         </v-col>
 
+        <!-- character skill -->
+        <v-col cols="12" md="6" v-if="isExpanded">
+          <v-text-field
+            v-if="isSelfDefinedSkill"
+            label="技能敘述-其他"
+            v-model="skillKeyword"
+            clearable
+            prepend-inner-icon="mdi-magnify"
+            append-inner-icon="mdi-dots-horizontal"
+            @click:append-inner="changeSkillKeywordType">
+          </v-text-field>
+          <v-autocomplete
+            v-else
+            label="技能敘述"
+            v-model="skillKeyword"
+            prepend-inner-icon="mdi-magnify"
+            append-inner-icon="mdi-dots-horizontal"
+            :items="skillDescription"
+            item-value="name"
+            clearable
+            :item-props="(item) => {return {'title': item.name, 'subtitle': item.description}}"
+
+            @click:append-inner="changeSkillKeywordType"
+          >
+          </v-autocomplete>
+        </v-col>
+      </v-row>
+    </v-col>
+
+    <!-- expand button -->
+    <v-col cols="2" md="1">
+      <v-tooltip
+        location="left"
+      >
+        <template v-slot:activator="{ props }">
+          <div v-bind="props">
+            <v-btn icon @click="changeExpand">
+              <v-icon v-if="isExpanded">
+                mdi-chevron-up
+              </v-icon>
+              <v-icon v-else>
+                mdi-chevron-down
+              </v-icon>
+            </v-btn>
+          </div>
+        </template>
+        <span v-if="isExpanded">收合搜尋欄</span>
+        <span v-else>展開搜尋欄</span>
+      </v-tooltip>
+    </v-col>
+
+    <v-col cols="12" v-if="isExpanded">
+      <v-row>
         <!-- attribute(color) -->
         <v-col cols="12" md="auto" lg="auto">
           <v-btn-toggle
@@ -92,11 +115,12 @@
           </v-btn-toggle>
         </v-col>
       </v-row>
-
+    </v-col>
+  </v-row>
 </template>
 
 <script setup>
-  import { ref, watch } from 'vue';
+  import { onMounted, ref, watch } from 'vue';
   import enumRelated from '@/common/scriptFile/enumRelated'
   import commonEnum from '@/common/scriptFile/commonEnum'
 
@@ -136,6 +160,16 @@
   watch(() => [nameKeyword.value, skillKeyword.value], () => {
       filter()
     }, {deep: true})
+
+  const isExpanded = ref(true);
+  onMounted(() => {
+    isExpanded.value = localStorage.searchFieldExpanded === 'true';
+  })
+
+  function changeExpand() {
+    isExpanded.value = !isExpanded.value;
+    localStorage.searchFieldExpanded = isExpanded.value;
+  }
 
 </script>
 
