25f2e1d0bffe006ef2be9c047789527dd9e17e01
tsai1247 <jeff29cc91079@gmail.com>
2024-03-13 14:41:33 +0800

    feat: add version info page

---
+++ src/components/versioninfo/catCityVersionView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    貓之城版本資訊
+  <!-- <v-row>
+      <v-col
+        v-for="i in 6"
+        :key="i"
+        cols="12"
+        md="4"
+      >
+        <v-img
+          :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+          :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+          aspect-ratio="1"
+        ></v-img>
+      </v-col>
+    </v-row> -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
