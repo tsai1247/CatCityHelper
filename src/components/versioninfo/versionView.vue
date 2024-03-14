02ca93cb354a7f1e432d6d3e7f0931df93083faa
tsai1247 <jeff29cc91079@gmail.com>
2024-03-14 22:47:05 +0800

    feat: swap priority fro version tabs

--- src/components/versioninfo/versionView.vue
+++ src/components/versioninfo/versionView.vue
@@ -4,15 +4,15 @@
       v-model="tabIndex"
       align-tabs="center"
     >
-      <v-tab :value="0">圖鑑版本資訊</v-tab>
-      <v-tab :value="1">貓之城版本資訊</v-tab>
+      <v-tab :value="0">貓之城版本資訊</v-tab>
+      <v-tab :value="1">圖鑑版本資訊</v-tab>
     </v-tabs>
     <v-window v-model="tabIndex">
       <v-window-item
         :value="0"
       >
         <v-container fluid>
-          <book-version-view></book-version-view>
+          <cat-city-version-view></cat-city-version-view>
         </v-container>
       </v-window-item>
 
@@ -20,9 +20,10 @@
         :value="1"
       >
         <v-container fluid>
-          <cat-city-version-view></cat-city-version-view>
+          <book-version-view></book-version-view>
         </v-container>
       </v-window-item>
+
     </v-window>
   </div>
 </template>
@@ -32,7 +33,7 @@ import { ref } from 'vue';
 import bookVersionView from './bookVersionView.vue';
 import catCityVersionView from './catCityVersionView.vue';
 
-const tabIndex = ref(1);
+const tabIndex = ref(0);
 
 </script>
 
