b62c8e3d0bb5e6c2206fd5d1db205b1a96362268
tsai1247 <jeff29cc91079@gmail.com>
2024-03-16 16:25:42 +0800

    chore: change cato folder name

--- src/common/images.js
+++ src/common/images.js
@@ -10,13 +10,13 @@ function getImageDictionary(folder) {
 const characterImageFolder = Object.values(import.meta.glob('@/assets/character/image/*.{png,jpg,jpeg,PNG,JPEG}', { eager: true, as: 'url' }))
 const characterImages = getImageDictionary(characterImageFolder);
 
-const catoImageFolder_SSR = Object.values(import.meta.glob('@/assets/catoImages/image/SSR/*.{png,jpg,jpeg,PNG,JPEG}', { eager: true, as: 'url' }))
+const catoImageFolder_SSR = Object.values(import.meta.glob('@/assets/cato/image/SSR/*.{png,jpg,jpeg,PNG,JPEG}', { eager: true, as: 'url' }))
 const catoImages_SSR = getImageDictionary(catoImageFolder_SSR);
 
-const catoImageFolder_SR = Object.values(import.meta.glob('@/assets/catoImages/image/SR/*.{png,jpg,jpeg,PNG,JPEG}', { eager: true, as: 'url' }))
+const catoImageFolder_SR = Object.values(import.meta.glob('@/assets/cato/image/SR/*.{png,jpg,jpeg,PNG,JPEG}', { eager: true, as: 'url' }))
 const catoImages_SR = getImageDictionary(catoImageFolder_SR);
 
-const catoImageFolder_R = Object.values(import.meta.glob('@/assets/catoImages/image/R/*.{png,jpg,jpeg,PNG,JPEG}', { eager: true, as: 'url' }))
+const catoImageFolder_R = Object.values(import.meta.glob('@/assets/cato/image/R/*.{png,jpg,jpeg,PNG,JPEG}', { eager: true, as: 'url' }))
 const catoImages_R = getImageDictionary(catoImageFolder_R);
 
 export default {
