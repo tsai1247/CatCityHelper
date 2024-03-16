50c45d91dde320afe131328bfe95d843c14e51d4
tsai1247 <jeff29cc91079@gmail.com>
2024-03-16 18:10:59 +0800

    feat: expand sidebar on hover

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         <v-spacer></v-spacer>
       </v-app-bar>
 
-    <v-navigation-drawer :rail="isexpended" permanent>
+    <v-navigation-drawer :rail="isexpended" expand-on-hover permanent>
       <v-list-item prepend-icon="mdi-home" class="mt-2" title="首頁" link to="/Home"></v-list-item>
       <v-list-item prepend-icon="mdi-account" class="mt-2" title="角色圖鑑" link to="/Character"></v-list-item>
       <v-list-item prepend-icon="mdi-cat" class="mt-2" title="貓球圖鑑" link to="/Cato"></v-list-item>
