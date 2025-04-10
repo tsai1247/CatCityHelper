6d3dcd48d3068148832f47f9a3e1e392eb313c7c
ChaosOp <tp6ru8bj6@gmail.com>
2025-04-10 11:29:25 +0800

    feat: add 滌盡凡塵 skills feat: add 滌盡凡塵 character image

--- src/common/versions.js
+++ src/common/versions.js
@@ -1,5 +1,13 @@
 const bookVersionList = [
   {
+    "version": "1.17.1.0",
+    "title": "",
+    "description": [
+      "【新增】新角色技能與數據：滌盡凡塵",
+      "【新增】角色圖片：滌盡凡塵",
+    ]
+  },
+  {
     "version": "1.17.0.0",
     "title": "",
     "description": [
